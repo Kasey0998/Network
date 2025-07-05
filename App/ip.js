0af1e17a92be08da96bfa5e278f323def42103ee
Krishna Sharma <kasey.gnr@icloud.com>
2025-07-05 19:41:20 +0100

    removed ip.js

--- App/ip.js
+++
@@ -1,9 +0,0 @@
-fetch('https://api.ipify.org?format=json')
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById('ip').innerText = `Server IP: ${data.ip}`;
-  })
-  .catch(error => {
-    document.getElementById('ip').innerText = 'Server IP: Unable to fetch IP';
-    console.error('Error fetching IP:', error);
-  });
